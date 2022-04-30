d50d42bcd8fc22b36cd8e1be155d8096bdee643d
Henry Xiao <62553342+henry123-boy@users.noreply.github.com>
2022-04-30 11:42:32 +0800

    Add files via upload

---
+++ model/utils.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+import pdb
+from functools import cmp_to_key
+def isPixelAnExtremum(first_subimage, second_subimage, third_subimage, threshold):
+    """Return True if the center element of the 3x3x3 input array is strictly greater than or less than all its neighbors, False otherwise
+    """
+    center_pixel_value = second_subimage[1, 1]
+    if abs(center_pixel_value) > threshold:
+        if center_pixel_value > 0:
+            return (center_pixel_value >= first_subimage).all() and \
+                   (center_pixel_value >= third_subimage).all() and \
+                   (center_pixel_value >= second_subimage[0, :]).all() and \
+                   (center_pixel_value >= second_subimage[2, :]).all() and \
+                   center_pixel_value >= second_subimage[1, 0] and \
+                   center_pixel_value >= second_subimage[1, 2]
+        elif center_pixel_value < 0:
+            return (center_pixel_value <= first_subimage).all() and \
+                   (center_pixel_value <= third_subimage).all() and \
+                   (center_pixel_value <= second_subimage[0, :]).all() and \
+                   (center_pixel_value <= second_subimage[2, :]).all() and \
+                   center_pixel_value <= second_subimage[1, 0] and \
+                   center_pixel_value <= second_subimage[1, 2]
+    return False
+
+def computeGradientAtCenterPixel(pixel_array):
+    """Approximate gradient at center pixel [1, 1, 1] of 3x3x3 array using central difference formula of order O(h^2), where h is the step size
+    """
+    # With step size h, the central difference formula of order O(h^2) for f'(x) is (f(x + h) - f(x - h)) / (2 * h)
+    # Here h = 1, so the formula simplifies to f'(x) = (f(x + 1) - f(x - 1)) / 2
+    # NOTE: x corresponds to second array axis, y corresponds to first array axis, and s (scale) corresponds to third array axis
+    dx = 0.5 * (pixel_array[1, 1, 2] - pixel_array[1, 1, 0])
+    dy = 0.5 * (pixel_array[1, 2, 1] - pixel_array[1, 0, 1])
+    ds = 0.5 * (pixel_array[2, 1, 1] - pixel_array[0, 1, 1])
+    return np.array([dx, dy, ds])
+
+def unpackOctave(keypoint):
+    """Compute octave, layer, and scale from a keypoint
+    """
+    octave = keypoint.octave & 255
+    layer = (keypoint.octave >> 8) & 255
+    if octave >= 128:
+        octave = octave | -128
+    scale = 1 / np.float32(1 << octave) if octave >= 0 else np.float32(1 << -octave)
+    return octave, layer, scale
+def compareKeypoints(keypoint1, keypoint2):
+    """Return True if keypoint1 is less than keypoint2
+    """
+    if keypoint1.pt[0] != keypoint2.pt[0]:
+        return keypoint1.pt[0] - keypoint2.pt[0]
+    if keypoint1.pt[1] != keypoint2.pt[1]:
+        return keypoint1.pt[1] - keypoint2.pt[1]
+    if keypoint1.size != keypoint2.size:
+        return keypoint2.size - keypoint1.size
+    if keypoint1.angle != keypoint2.angle:
+        return keypoint1.angle - keypoint2.angle
+    if keypoint1.response != keypoint2.response:
+        return keypoint2.response - keypoint1.response
+    if keypoint1.octave != keypoint2.octave:
+        return keypoint2.octave - keypoint1.octave
+    return keypoint2.class_id - keypoint1.class_id
+def removeDuplicateKeypoints(keypoints):
+    """Sort keypoints and remove duplicate keypoints
+    """
+    if len(keypoints) < 2:
+        return keypoints
+
+    keypoints.sort(key=cmp_to_key(compareKeypoints))
+    unique_keypoints = [keypoints[0]]
+
+    for next_keypoint in keypoints[1:]:
+        last_unique_keypoint = unique_keypoints[-1]
+        if last_unique_keypoint.pt[0] != next_keypoint.pt[0] or \
+           last_unique_keypoint.pt[1] != next_keypoint.pt[1] or \
+           last_unique_keypoint.size != next_keypoint.size or \
+           last_unique_keypoint.angle != next_keypoint.angle:
+            unique_keypoints.append(next_keypoint)
+    return unique_keypoints
+def convertKeypointsToInputImageSize(keypoints):
+    """Convert keypoint point, size, and octave to input image size
+    """
+    converted_keypoints = []
+    for keypoint in keypoints:
+        keypoint.pt = tuple(0.5 * np.array(keypoint.pt))
+        keypoint.size *= 0.5
+        keypoint.octave = (keypoint.octave & ~255) | ((keypoint.octave - 1) & 255)
+        converted_keypoints.append(keypoint)
+    return converted_keypoints
+def computeHessianAtCenterPixel(pixel_array):
+    """Approximate Hessian at center pixel [1, 1, 1] of 3x3x3 array using central difference formula of order O(h^2), where h is the step size
+    """
+    # With step size h, the central difference formula of order O(h^2) for f''(x) is (f(x + h) - 2 * f(x) + f(x - h)) / (h ^ 2)
+    # Here h = 1, so the formula simplifies to f''(x) = f(x + 1) - 2 * f(x) + f(x - 1)
+    # With step size h, the central difference formula of order O(h^2) for (d^2) f(x, y) / (dx dy) = (f(x + h, y + h) - f(x + h, y - h) - f(x - h, y + h) + f(x - h, y - h)) / (4 * h ^ 2)
+    # Here h = 1, so the formula simplifies to (d^2) f(x, y) / (dx dy) = (f(x + 1, y + 1) - f(x + 1, y - 1) - f(x - 1, y + 1) + f(x - 1, y - 1)) / 4
+    # NOTE: x corresponds to second array axis, y corresponds to first array axis, and s (scale) corresponds to third array axis
+    center_pixel_value = pixel_array[1, 1, 1]
+    dxx = pixel_array[1, 1, 2] - 2 * center_pixel_value + pixel_array[1, 1, 0]
+    dyy = pixel_array[1, 2, 1] - 2 * center_pixel_value + pixel_array[1, 0, 1]
+    dss = pixel_array[2, 1, 1] - 2 * center_pixel_value + pixel_array[0, 1, 1]
+    dxy = 0.25 * (pixel_array[1, 2, 2] - pixel_array[1, 2, 0] - pixel_array[1, 0, 2] + pixel_array[1, 0, 0])
+    dxs = 0.25 * (pixel_array[2, 1, 2] - pixel_array[2, 1, 0] - pixel_array[0, 1, 2] + pixel_array[0, 1, 0])
+    dys = 0.25 * (pixel_array[2, 2, 1] - pixel_array[2, 0, 1] - pixel_array[0, 2, 1] + pixel_array[0, 0, 1])
+    return np.array([[dxx, dxy, dxs],
+                  [dxy, dyy, dys],
+                  [dxs, dys, dss]])
